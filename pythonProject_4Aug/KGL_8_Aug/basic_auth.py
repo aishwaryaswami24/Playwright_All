97d671d0c895d4d900678453ff67fcd3e262e2e6
Aishwarya SwamiNagure <swamiaishwarya24@gmail.com>
2024-12-05 15:21:13 +0530

    http basic auth

--- pythonProject_4Aug/KGL_8_Aug/basic_auth.py
+++ pythonProject_4Aug/KGL_8_Aug/basic_auth.py
@@ -1,21 +1,37 @@
+# #general way
+#
+# from playwright.sync_api import sync_playwright,expect
+# with sync_playwright() as p:
+#     browser=p.chromium.launch(headless=False)
+#     context=browser.new_context()
+#     page=context.new_page()
+#     page.goto('https://practicetestautomation.com/practice-test-login/')
+#
+#     page.fill('#username','student')
+#     page.fill('#password','Password123')
+#     page.locator('#submit').click()
+#
+#     # result=page.locator('//strong[text()="Congratulations student. You successfully logged in!"]').inner_text()
+#     # print(result)
+#
+#     result = page.locator('//strong[text()="Congratulations student. You successfully logged in!"]')
+#     expect(result).to_have_text('Congratulations student. You successfully logged in!')
+#
+#     page.wait_for_timeout(2000)
+#
+#     context.close()
+#     browser.close()
+
+#http auth
 from playwright.sync_api import sync_playwright,expect
 with sync_playwright() as p:
     browser=p.chromium.launch(headless=False)
-    context=browser.new_context()
+    context=browser.new_context(http_credentials={'username':'admin','password':'admin'})
     page=context.new_page()
-    page.goto('https://practicetestautomation.com/practice-test-login/')
-
-    page.fill('#username','student')
-    page.fill('#password','Password123')
-    page.locator('#submit').click()
-
-    # result=page.locator('//strong[text()="Congratulations student. You successfully logged in!"]').inner_text()
-    # print(result)
-
-    result = page.locator('//strong[text()="Congratulations student. You successfully logged in!"]')
-    expect(result).to_have_text('Congratulations student. You successfully logged in!')
-
+    page.goto('https://the-internet.herokuapp.com/basic_auth')
     page.wait_for_timeout(2000)
+    expect(page.locator('#content p')).to_have_text('Congratulations! You must have the proper credentials.')
+
 
     context.close()
     browser.close()
