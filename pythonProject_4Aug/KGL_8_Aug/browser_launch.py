3deef89844f23aed8690ed47e829b394cd5ec965
Aishwarya SwamiNagure <swamiaishwarya24@gmail.com>
2024-12-23 19:40:34 +0530

    Practice

--- pythonProject_4Aug/KGL_8_Aug/browser_launch.py
+++ pythonProject_4Aug/KGL_8_Aug/browser_launch.py
@@ -1,10 +1,31 @@
+# from playwright.sync_api import sync_playwright
+# with sync_playwright() as p:
+#     browser = p.chromium.launch(headless=False)
+#     page = browser.new_page()
+#     page.goto("https://www.google.co.in/")
+#     print("Chromium browser opened successfully")
+#     print(page.title())
+#     page.get_by_label("Search", exact=True)
+#     page.wait_for_timeout(3000)
+#     browser.close()
+
+
 from playwright.sync_api import sync_playwright
 with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto("https://www.google.co.in/")
-    print("Chromium browser opened successfully")
-    print(page.title())
-    page.get_by_label("Search", exact=True)
-    page.wait_for_timeout(3000)
+    browser=p.chromium.launch(headless=False)
+    page=browser.new_page()
+
+    page.goto('https://letzautomate.github.io/samples/Login.html')
+
+    username=page.wait_for_selector('//label[text()="Username"]/following-sibling::div//input')
+    username.type('admin')
+
+    password=page.wait_for_selector('//label[text()="Password"]/following-sibling::div//input')
+    password.type('admin')
+
+    login=page.wait_for_selector('//button[@type="submit"]')
+    login.click()
+
+    page.wait_for_timeout(2000)
+
     browser.close()
