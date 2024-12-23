94eba2c1451d76b2363844ae6b7096850036b687
Aishwarya SwamiNagure <swamiaishwarya24@gmail.com>
2024-12-23 18:30:11 +0530

    Practice

--- pythonProject_4Aug/KGL_8_Aug/webtable_fetchvalues.py
+++ pythonProject_4Aug/KGL_8_Aug/webtable_fetchvalues.py
@@ -1,12 +1,23 @@
 from playwright.sync_api import sync_playwright
+
+from Playwright.code1 import first_value
+
 with sync_playwright() as p:
     browser=p.chromium.launch(headless=False)
     context=browser.new_context()
     page=context.new_page()
+
     page.goto('https://letzautomate.github.io/samples/eCommerce.html')
     page.wait_for_timeout(1000)
+
     column_index=3
     column_three_values=page.locator(f'//table[@id="productTable"]//tbody/tr/td[{column_index}]').all_text_contents()
     print(column_three_values)
 
     browser.close()
+
+    #Fetch only one value from the output list
+
+    first_value=column_three_values[0] if column_three_values else None
+    print(first_value)
+
